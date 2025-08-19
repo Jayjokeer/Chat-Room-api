62282a7eefbf42479078282ad51c92f326f67cee
Jayjokeer <johntommokaa@gmail.com>
2025-08-19 13:27:55 +0100

    installing dot env

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
+// import routes from './routes/index.routes';
 import globalErrorHandler from './errors/error-handler';
 import AppError from './errors/error';
 
 dotenv.config();
-require('./db');
 
 const app = express();
 app.use(express.json());
 
+// app.use('/api/v1', routes);
 
 app.use(globalErrorHandler); 
-app.all('*', (req, res, next) => {
-  next(new AppError(`Cannot find ${req.originalUrl} on this server`, 404));
-});
+// app.all('*', (req, res, next) => {
+//   next(new AppError(`Cannot find ${req.originalUrl} on this server`, 404));
+// });
 
 export default app;
